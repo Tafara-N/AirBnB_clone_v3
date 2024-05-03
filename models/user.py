c5a419d2d4bec683334edfafc1b2a8c1eb4998c4
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-03 20:33:09 +0200

    Representation of a user.

--- models/user.py
+++ models/user.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-""" holds class User"""
+
+"""
+Holds class User
+"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """
+    Representation of a user
+    """
+
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -25,5 +33,16 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+        Initializing the user
+        """
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """
+        Setting a password encrypted by md5
+        """
+
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)

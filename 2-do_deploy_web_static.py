67d69360ed6a8747beb7fa60b3cfef93d486fc6b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-03 17:41:55 +0200

    Distrubiting archives on the web servers based on the `1-pack_web_static.py` file.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
@@ -10,7 +11,10 @@ env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """
+    Distributes an archive to the web servers
+    """
+
     if exists(archive_path) is False:
         return False
     try:
@@ -26,5 +30,5 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
